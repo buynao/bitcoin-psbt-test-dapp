073f14c98d01140cff6326ae6cada8412f0c3040
law <law.liu@token.im>
2024-06-27 17:34:53 +0800

    add devider

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import Connect from './components/Connect';
 import { WalletProvider } from './wallet/wallet_provider';
 import BabylonStaking from './components/BabylonStaking';
 import SignPsbt from './components/SignPsbt';
-
+import { Divider } from 'antd';
 function App() {
   const [btcWallet, setWallet] = useState<WalletProvider | null>(null);
-  console.log('>>>>>>>>winodw', window);
-  console.log('>>>>>>>buffer', Buffer.from('xxx'));
   return (
     <div style={{ textAlign: 'center', margin: '30px' }}>
       <Connect btcWallet={btcWallet} setWallet={setWallet} />
+      <Divider />
       <BabylonStaking btcWallet={btcWallet} />
+      <Divider />
       <SignPsbt btcWallet={btcWallet} />
     </div>
   );
